5bcbf92c18b649a355fe88836f3d256af5a80fca
Muhammad Shayan <mshayanshafi@gmail.com>
2019-04-06 16:22:08 -0700

    First push

---
+++ style_decoder/sentiment_example.sh
@@ -0,0 +1,2 @@
+
+python translate.py -model ../models/translation/english_french/english_french.pt -src ../data/sentiment/sentiment.train.0 -output ../data/sentiment/sentiment.train.fr -replace_unk $true
