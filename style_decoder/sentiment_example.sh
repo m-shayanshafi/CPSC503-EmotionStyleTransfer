569a939fe52c8a4ae2e94a9a9237ede2e18e05d1
Muhammad Shayan <mshayanshafi@gmail.com>
2019-04-06 16:40:22 -0700

    Rearranging

--- style_decoder/sentiment_example.sh
+++
@@ -1,2 +0,0 @@
-
-python translate.py -model ../models/translation/english_french/english_french.pt -src ../data/sentiment/sentiment.train.0 -output ../data/sentiment/sentiment.train.fr -replace_unk $true
